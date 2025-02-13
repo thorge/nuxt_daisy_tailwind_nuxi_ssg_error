af999612c1cde982e58a35d40da4331dd67f17bf
Thorge Petersen <petersen@rz.uni-kiel.de>
2025-02-13 13:36:00 +0100

    Added example breadcrumb

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
   <MainNavbar />
   <main class="p-6">
+    <div class="breadcrumbs text-sm">
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>Documents</a></li>
+        <li>Add Document</li>
+      </ul>
+    </div>
     <div class="container max-w-5xl mx-auto">
       <NuxtPage />
     </div>
